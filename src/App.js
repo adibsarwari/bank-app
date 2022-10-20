077e493a99d825859cceb316be4efbc52abd8983
Mddanishsarwari <sam9445stark@gmail.com>
2022-10-20 14:51:19 +0530

    updated Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+import {Home} from "./Components/Home"
+import { Nav } from "./Components/Nav";
+import {Cards} from "./Components/Cards/Cards"
+import { Banking } from "./Components/Banking/Banking";
+import { Support } from "./Components/Support";
+import {Registration} from "./Components/Banking/Registration"
+import "./Components/Common.css"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Loans from "./Components/Loans/Loans";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="main">
+  <BrowserRouter>
+  <Nav/>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/banking" element={<Banking/>} />
+    <Route path="/cards" element={<Cards/>} />
+    <Route path="/loans" element={<Loans/>} />
+    <Route path="/support" element={<Support/>} />
+    {/* <Route path="/register" element={<Registration/>} /> */}
+    <Route path="/register" element={<Registration/>} />
+    
+  </Routes>
+  </BrowserRouter>
+  
+   </div>
   );
 }
 
 export default App;
+ 
